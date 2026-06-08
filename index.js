172e94b02bbef6b66c7343fbdd7b7f502d1b3bd6
Denis Kibichiy <deniskibichiy@outlook.com>
2026-06-08 21:02:14 +0300

    build the initial implementation of recursive fibonacci

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function fibs(n) {
         if (i == 0) {
             array.push(0);
         } else if ( i == 1 || i == 2) {
-            array.push(1)
+            array.push(1);
         } else {
             array.push((array[array.length-1]) + array[array.length-2]);
         }
@@ -16,16 +16,27 @@ function fibs(n) {
 
 (fibs(20));
 
-function recursFib(n) {
+function fibElement(n) {
     if (n == 0) {
         return 0;
     } else if (n == 1) {
         return 1;
     } else {
-        return recursFib(n-1) + recursFib(n - 2);
+        return fibElement(n-1) + fibElement(n - 2);
     }
 }
 
-console.log(recursFib(10));
+console.log(fibElement(10));
+
+function recursFib(n) {
+    if (n == 0) {
+        return [0];
+    } else if (n == 1) {
+        return [0,1];
+    } else {
+        const array = recursFib(n-1);
+        array.push(array[array.length-1] + array[array.length-2]);
+    }
+}
 
 //export {recursFib}
